619ac95361752bc5bda853b3cf42b79c5fafdce7
trinkner <dbtrinkner@gmail.com>
2023-07-18 15:11:37 -0600

    tanakai tumblr cleanup

--- tanakai/tumblr_spider.rb
+++ tanakai/tumblr_spider.rb
@@ -20,7 +20,6 @@ class TumblrSpider < Tanakai::Base
     @account = Hypertext.find_by!(url:  url = (url.sub! '/sitemap1.xml', ''))
     @source_url_id = @account.source_url_id
     @hypertext_id = @account.id
-    @author = @account.name
 
     response.css("url").each do |a|
       @url = a.css('loc').text
@@ -36,17 +35,21 @@ class TumblrSpider < Tanakai::Base
   def parse_repo_page(response, url:, data: {})
 
     # IMAGES
-    xp_img_reblog =       "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[2]/div/div/button/span/figure/div/img"
-    xp_img_standard =     "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[1]/button/span/figure/div/img"
-    xp_img_tiny =         "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div/figure/div/img"
-    xp_img_tiny_reblog =  "//*[@id='base-container']/div[2]/div[2]/div/div/div/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[2]/div/div[1]/div/button/span/figure/div/img"
-    xp_img_medium =       "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[2]/div/div[1]/button/span/figure/div/img"
+    xp_img_reblog =      "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[2]/div/div/button/span/figure/div/img"
+    xp_img_standard =    "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[1]/button/span/figure/div/img"
+    xp_img_tiny =        "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div/figure/div/img"
+    xp_img_tiny_reblog = "//*[@id='base-container']/div[2]/div[2]/div/div/div/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[2]/div/div[1]/div/button/span/figure/div/img"
+    xp_img_medium =      "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[2]/div/div[1]/button/span/figure/div/img"
 
-    xp_descr = "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[2]/p"
-    xp_tags = "/html/body/div[1]/div/div[2]/div[2]/div/div/div/main/div/div/div/div[2]/div/div/div/article/div[2]/div/div/a"
-    xp_auth = "/html/body/div/div/div[2]/div[2]/div/div/div/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[1]/div[1]/div[2]/div/div/span/span/span/a/div"
-    xp_reblog_auth = "//*[@id='base-container']/div[2]/div[2]/div/div/div/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[1]/div[1]/div/div/div/span/div"
+    xp_descr =           "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[2]/p"
+    xp_tags =            "/html/body/div[1]/div/div[2]/div[2]/div/div/div/main/div/div/div/div[2]/div/div/div/article/div[2]/div/div/a"
+    xp_auth =            "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/header/div/div[1]/div[1]/div/span[1]/a"
+    xp_reblog_auth =     "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[1]/div[1]/div/div/div/span/div"
+    xp_reblog_two_auth =     "//*[@id='base-container']/div[2]/div[2]/div/div/div[1]/main/div/div/div/div[2]/div/div/div/article/div[1]/div/span/div/div[1]/div[1]/div[2]/div/div/span/span/span/a/div"
 
+
+
+    # IMG LOCATING, EXTRACTION
     # size standard
     if response.xpath(xp_img_standard).attr('srcset')
       img_html = response.xpath(xp_img_standard)
@@ -64,48 +67,64 @@ class TumblrSpider < Tanakai::Base
       img_html = response.xpath(xp_img_tiny_reblog)
     end
     
-    if !img_html.empty?
+    if !img_html.nil?
+      # IMAGE FILE
+      file_path = ""
       puts(img_html.attr('srcset').text.scan(/\bhttps?:\/\/[^\s]+\.(?:jpg|gif|png|pnj|gifv)\b/).last)
       tempfile = Down.download(img_html.attr('srcset').text.scan(/\bhttps?:\/\/[^\s]+\.(?:jpg|gif|png|pnj|gifv)\b/).last)
-      @imgPath = tempfile.original_filename
+      file_path = tempfile.original_filename
       FileUtils.mv(tempfile.path, "/home/pin/crystal_hair/crystal_hair_ui_vueCL/public/feed/#{tempfile.original_filename}")
 
       # DESCRIPTIOM
+      description = ""
       descr = response.xpath(xp_descr)
       if !descr.empty?
-        @descri = descr.text
+        description = descr.text
       end
       
       # HASHTAGS
+      hashtags = ""
       tags = response.xpath(xp_tags)
       if !tags.empty?
-        @hashtags = tags.text
+        hashtags = tags.text
       end
 
       # POST ACCOUNT
+      author = ""
       auth = response.xpath(xp_auth)
       if !auth.empty?
-        @author = tags.text
-      else
-        auth = response.xpath(xp_reblog_auth)
-        if !auth.empty?
-          @author = tags.text
-        end
+        author = auth.text
+      end
+      auth = response.xpath(xp_reblog_auth)
+      if !auth.empty?
+        author = auth.text
+      end
+      auth = response.xpath(xp_reblog_two_auth)
+      if !auth.empty?
+        author = auth.text
       end
 
-      if Kernal.exists?(file_path: @imgPath)
+      # API POST
+      if Kernal.exists?(file_path: file_path)
         puts('kernal exists')
       else
         puts('kernal absent')
+        puts(@source_url_id)
+        puts(@hypertext_id)
+        puts("file_path= " + file_path)
+        puts("file_name= " + file_path)
+        puts("description= " + description)
+        puts("hashtags= " + hashtags)
+        puts("author= " + author)
+        puts("url= " + url)
         @link = Kernal.create(
           source_url_id:@source_url_id, 
           hypertext_id:@hypertext_id, 
-          file_path:@imgPath, 
-          file_name:@imgPath, 
-          description:@descri, 
-          hashtags:@hashtags, 
-          author:@author, 
-          time_posted:@time_posted, 
+          file_path:file_path, 
+          file_name:file_path, 
+          description:description, 
+          hashtags:hashtags, 
+          author:author, 
           url:url
         )
       end
