8a7ade95362956089ccbd36ca54dd021c7c98049
trinkner <dbtrinkner@gmail.com>
2023-09-01 23:42:39 +0000

    are.na scrape now iterates channels and channel blocks

--- app/tanakai/ArenaApi.rb
+++ app/tanakai/ArenaApi.rb
@@ -1,12 +1,34 @@
-
 require './config/environment/'
+
 Arena.configure do |config|
     config.access_token = 'XC3fJj8wxLJIi0Bt39sxsoMpJ_39WpQtrLC7tD9ACwg'
 end
-contents = Arena.user_channels('494214', options={page: 0}).channels[0].contents
 
-e = Arena.channel('1687235', options={}).content
-puts e
 
-puts(Arena.user_channels('494214', options={}).channels[0].id)
-puts(Arena.user_channels('494214', options={}).channels[0].title)
+flag = false
+i = 0
+e = Arena.user_channels('494214', options={page: i}).channels
+while e.length > 0
+  e.each do |a| 
+    title = a.title
+    chId = a.id
+    puts(title)
+    if !Mixtape.exists?(name: title)
+      @mix = Mixtape.create(
+        name: title
+      )
+    end
+
+    n = 0
+    b = Arena.channel(chId, options={page: n}).contents
+    while b.length > 0
+      b.each do |a| 
+        puts(a.class)
+      end
+      n = n + 1
+      b = Arena.channel(chId, options={page: n}).contents
+    end
+  end
+  i = i + 1
+  e = Arena.user_channels('494214', options={page: i}).channels
+end
