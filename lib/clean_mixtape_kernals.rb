284503d7bb5d05eff306e5dd930ce23782b127eb
Dylan Trinkner <dbtrinkner@gmail.com>
2024-01-14 22:50:15 +0000

    removed repeated files

--- lib/clean_mixtape_kernals.rb
+++
@@ -1,9 +0,0 @@
-require './config/environment/'
-
-Mixtape.all.each do |mix|
-  mix.content.each do |k|
-    if !Kernal.exists?(k)
-      mix.update_attribute(:content, (mix.content - [k]))
-    end
-  end
-end
